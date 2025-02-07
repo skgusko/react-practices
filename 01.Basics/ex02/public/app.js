18feb9a89a002964aa2c8bc17667f16f224e9ea0
Konahyeon <koo@inha.edu>
2025-02-07 13:33:15 +0900

    react #1

---
+++ 01.Basics/ex02/public/app.js
@@ -0,0 +1,7 @@
+// App 함수 정의
+function App() {
+    const App = document.createElement('div');
+    App.textContent = "Hello World";
+
+    return App;
+}
---
+++ 01.Basics/ex02/public/index.js
@@ -0,0 +1,4 @@
+// App 함수 사용
+document
+    .getElementById("root")
+    .appendChild(App());
