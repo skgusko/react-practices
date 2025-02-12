18cb3a15d7ac30de22b07eefd7112b606eea76ff
Konahyeon <koo@inha.edu>
2025-02-12 15:17:32 +0900

    JSX

--- 02.JSX Tutorials/ex04/src/App.js
+++ 02.JSX Tutorials/ex04/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Header from './Header';
 
 function App() {
-    /*
+   
+    const refDiv = useRef(null); //{current: null}
+    const [test, setTest] = useState('hello');
     return (
-        <div id="App">
-            <Header />
-            <Contents />
+        <div 
+            id="App"
+            ref={refDiv}>
+            {'Test'}
         </div>
     );
-    */
-   
-    return React.createElement('div', {
-        id: 'App'
-    }, React.createElement(Header, null), React.createElement(Contents, null));
+    
+    // return React.createElement('div', {id: 'App'}, 'Test');
+    // return React.createElement('div', {id: 'App'}, React.createElement(Header, null), React.createElement(Contents, null));
 }
 
 export {App};
--- 02.JSX Tutorials/ex04/src/index.js
+++ 02.JSX Tutorials/ex04/src/index.js
@@ -4,7 +4,6 @@ import {App} from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+console.log(root);
