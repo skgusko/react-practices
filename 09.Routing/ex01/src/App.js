6d13a8ae54aa5e1fea6e72ac3e52062f225e9a26
Konahyeon <koo@inha.edu>
2025-02-20 09:38:01 +0900

    Routing

---
+++ 09.Routing/ex01/src/App.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import Main from "./component/Main";
+import Gallery from "./component/Gallery";
+import Guestbook from "./component/Guestbook";
+import Error404 from "./component/Error404";
+
+export default function App() {
+
+    const route = '/';
+
+    return (() => {
+        switch(route) {
+            case '' :
+            case '/' :
+                return <Main />;
+            case '/guestbook' :
+                return <Guestbook />;
+            case '/gallery':
+                return <Gallery />;
+            default :
+                return <Error404 />;                    
+        }
+    })();
+}
